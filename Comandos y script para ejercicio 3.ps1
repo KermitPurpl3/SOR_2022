2a1a22444be6ed340266e7999fcb9e1b0d741966
Un kebab mixto con de todo para llevar <91491739+CyberpunkEdgerunnersDavidMatrinezKebab@users.noreply.github.com>
2022-10-08 16:56:04 +0200

    Update Comandos y script para ejercicio 3.ps1

--- Comandos y script para ejercicio 3.ps1
+++ Comandos y script para ejercicio 3.ps1
@@ -2,15 +2,15 @@
 New-OrganizationalUnit "Nombre"
 
 #Crear usuarios
-New-ADUser -Name "David.segarra"-Path "OU=Ventas,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Macos.gonzalez" -Path "OU=Ventas,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Adrian.serreno" -Path "OU=Ventas,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Jaume.perez" -Path "OU=Ventas,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Eduardo.garcia" -Path "OU=Ventas,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Enrique.ballester" -Path "OU=Compras,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Maria.lopez" -Path "OU=Compras,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Pepe.llorca" -Path "OU=Compras,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Ramon.martinez" -Path "OU=Contabilidad,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Eva.tarrega" -Path "OU=Contabilidad,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Raul.millan" -Path "OU=Contabilidad,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Susana.sotorres" -Path "OU=Contabilidad,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "aso2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "David" -Surname "Segarra" -Path "OU=VENTAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
+New-ADUser -Name "Macos.gonzalez" -Path "OU=Ventas,OU=Empresa,OU=Empresa,OU=Empresa,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "Adrian.serreno" -Path "OU=Ventas,OU=Empresa,OU=Empresa,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "Jaume.perez" -Path "OU=Ventas,OU=Empresa,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "Eduardo.garcia" -Path "OU=Ventas,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "Enrique.ballester" -Path "OU=Compras,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "Maria.lopez" -Path "OU=Compras,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "Pepe.llorca" -Path "OU=Compras,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "Ramon.martinez" -Path "OU=Contabilidad,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "Eva.tarrega" -Path "OU=Contabilidad,OU=Empresa,OU=Empresa,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "Raul.millan" -Path "OU=Contabilidad,OU=Empresa,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "Susana.sotorres" -Path "OU=Contabilidad,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true

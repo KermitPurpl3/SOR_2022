14756fa9ad0f4992d347de8e8dea09d75831aa77
Un kebab mixto con de todo para llevar <91491739+CyberpunkEdgerunnersDavidMatrinezKebab@users.noreply.github.com>
2022-10-11 15:14:42 +0200

    Update Comandos y script para ejercicio 3.ps1

--- Comandos y script para ejercicio 3.ps1
+++ Comandos y script para ejercicio 3.ps1
@@ -3,17 +3,17 @@ New-OrganizationalUnit "Nombre"
 
 #Crear usuarios
 New-ADUser -Name "David" -Surname "Segarra" -Path "OU=VENTAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
-New-ADUser -Name "Macos" -Surname "Gonzalez" -Path "OU=VENTAS,OU=Empresa,,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Adrian" -Surname "Serrano" -Path "OU=VENTAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Jaume" -Surname "Perez" -Path "OU=VENTAS=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Eduardo" -Surname "Garcia" -Path "OU=VENTAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Enrique" -Surname "Ballester" -Path "OU=COMPRAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Maria" -Surname "Lopez" -Path "OU=COMPRAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Pepe" -Surname "Llorca" -Path "OU=COMPRAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Ramon" -Surname "Martinez" -Path "OU=CONTABiLIDAD,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Eva" -Surname "Tarrega" -Path "OU=CONTABiLIDAD,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Raul" -Surname "Millan" -Path "OU=CONTABILIDAD,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
-New-ADUser -Name "Susana" -Surname "Sotorres" -Path "OU=CONTABILIDAD,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $True -Enabled $true
+New-ADUser -Name "Marcos" -Surname "Gonzalez" -Path "OU=VENTAS,OU=Empresa,,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
+New-ADUser -Name "Adrian" -Surname "Serrano" -Path "OU=VENTAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
+New-ADUser -Name "Jaume" -Surname "Perez" -Path "OU=VENTAS=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
+New-ADUser -Name "Eduardo" -Surname "Garcia" -Path "OU=VENTAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
+New-ADUser -Name "Enrique" -Surname "Ballester" -Path "OU=COMPRAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
+New-ADUser -Name "Maria" -Surname "Lopez" -Path "OU=COMPRAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
+New-ADUser -Name "Pepe" -Surname "Llorca" -Path "OU=COMPRAS,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
+New-ADUser -Name "Ramon" -Surname "Martinez" -Path "OU=CONTABiLIDAD,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
+New-ADUser -Name "Eva" -Surname "Tarrega" -Path "OU=CONTABILIDAD,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
+New-ADUser -Name "Raul" -Surname "Millan" -Path "OU=CONTABILIDAD,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
+New-ADUser -Name "Susana" -Surname "Sotorres" -Path "OU=CONTABILIDAD,OU=Empresa,DC=GORGE,DC=LOCAL"-AccountPassword (ConvertTo-SecureString "A-S-O-2021" -AsPlainText -Force ) -ChangePasswordAtLogon $False -Enabled $true
 
 #EL SCRIPT QUE FUNCIONA
 Import-Csv C:\Users\Administrador.EMPRESA-DC1\Desktop\usuarios.csv | foreach-object {
